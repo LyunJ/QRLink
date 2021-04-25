c879853c57dc8267f7fbc5fb997febeb9184106a
LyunJ <tedlee0118@gmail.com>
2021-04-25 20:03:37 +0900

    add firebase

---
+++ firebase.js
@@ -0,0 +1,30 @@
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+        apiKey: "AIzaSyACgyozqDdSXgCC5R7Vww-IKp--WsHCkNw",
+        authDomain: "where-are-my-tabs.firebaseapp.com",
+        projectId: "where-are-my-tabs",
+        storageBucket: "where-are-my-tabs.appspot.com",
+        messagingSenderId: "421952925730",
+        appId: "1:421952925730:web:dd1e85bb86a548bb521a7b",
+        measurementId: "G-NM6KMKKHT4"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+      // Set the configuration for your app
+    // TODO: Replace with your project's config object
+    var config = {
+        apiKey: "AIzaSyACgyozqDdSXgCC5R7Vww-IKp--WsHCkNw",
+        authDomain: "where-are-my-tabs.firebaseapp.com",
+        // For databases not in the us-central1 location, databaseURL will be of the
+        // form https://[databaseName].[region].firebasedatabase.app.
+        // For example, https://your-database-123.europe-west1.firebasedatabase.app
+        databaseURL: "https://databaseName.firebaseio.com",
+        storageBucket: "where-are-my-tabs.appspot.com"
+    };
+    firebase.initializeApp(config);
+
+    // Get a reference to the database service
+    export var database = firebase.database();
